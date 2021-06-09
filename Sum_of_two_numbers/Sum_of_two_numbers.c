5e91ba48894afdd099eaae113fb59789956b8dff
d4rk3n <aravindmk@outlook.com>
2021-06-09 06:45:36 +0530

    .

---
+++ Sum_of_two_numbers/Sum_of_two_numbers.c
@@ -0,0 +1,20 @@
+/*Program to evaluate the sum of two numbers*/
+//Author : Aravind
+//Roll no. : 339
+//Date : 8/6/2021
+//Experiment : 1
+
+#include<stdio.h>
+
+int main(){
+    int a,b,s;
+    
+    printf("Enter the numbers : \n");
+    scanf("%d%d",&a,&b);
+    
+    s=a+b;
+    
+    printf("The sum is : %d",s);
+    
+    return(0);
+}
