78093625938f12f468fa7e3368b65f939ffd8ac1
d4rk3n <aravindmk@outlook.com>
2021-06-11 10:32:56 +0530

    .

--- Arithmetic_expression/Arithmetic_expression.c
+++ Arithmetic_expression/Arithmetic_expression.c
@@ -9,6 +9,7 @@ Experiment : 3
 
 int main(){
     int a,b,c,d,e,f,g,s;    //Declaring variables
+    
     printf("Enter the values of a,b,c,d,e,f,g\n");
     scanf("%d%d%d%d%d%d%d",&a,&b,&c,&d,&e,&f,&g);   //Recieving input from user
 
