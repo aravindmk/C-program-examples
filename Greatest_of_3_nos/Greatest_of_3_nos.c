b8845dbc5166017b81ccde764f4daaab4b9c6f69
d4rk3n <aravindmk@outlook.com>
2021-06-18 13:05:53 +0530

    .

--- Greatest_of_3_nos/Greatest_of_3_nos.c
+++ Greatest_of_3_nos/Greatest_of_3_nos.c
@@ -19,18 +19,18 @@ int main(){
 
     if (n1>n2){
         if (n1>n3){
-            printf("%d is the greatest number",n1);
+            printf("%d is the greatest number\n",n1);
         }
         else{
-            printf("%d is the greatest number",n3);
+            printf("%d is the greatest number\n",n3);
         }
     }
     else{
         if (n2>n3){
-            printf("%d is the greatest number",n2);
+            printf("%d is the greatest number\n",n2);
         }
         else{
-            printf("%d is the greatest number",n3);
+            printf("%d is the greatest number\n",n3);
         }
     }
     return(0);
